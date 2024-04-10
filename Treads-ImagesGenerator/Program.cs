dacf64dbd2a2fb36b0858635813b556dc2655c5d
What-I-pressed <AntonCymbaliuk.oof@gmail.com>
2024-04-10 16:46:59 +0300

    Add project files.

---
+++ Treads-ImagesGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    static string _dir = Directory.GetCurrentDirectory();
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(_dir);
+        Console.WriteLine("Threading is about to start");
+        Start();
+        Console.WriteLine("Pictures are downloaded");
+    }
+
+    static void Start()
+    {
+        Thread[] threads = new Thread[1000];
+        for(int i = 0; i < 1000; i++)
+        {
+            threads[i] = new Thread(GenerateAndSave);
+            threads[i].Start();
+        }
+        for(int i = 0; i < threads.Length; i++)
+        {
+            if (threads[i].ThreadState == ThreadState.Stopped) continue;
+            i = -1;
+            Thread.Sleep(2);
+        }
+    }
+
+    static void GenerateAndSave()
+    {
+        using(WebClient client = new WebClient())
+        {
+            client.DownloadFile(new Uri("http://loremflickr.com/320/240"), Path.Combine(_dir, "pictures", Guid.NewGuid().ToString() + ".jpg"));
+        }
+    }
+}
